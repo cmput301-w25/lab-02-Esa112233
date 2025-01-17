49d7ee5d72a606917ee2a086d1f1956b36129eb2
Esa112233 <esaabuzar@gmail.com>
2025-01-16 19:03:14 -0700

    Done

---
+++ app/src/main/java/com/example/listcity/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.listcity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView cityList;
+    ArrayAdapter<String> cityAdapter;
+    ArrayList<String> dataList;
+
+    Button Add_Button;
+
+    EditText input_field;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        Add_Button = findViewById(R.id.button7);
+
+
+
+
+
+
+        cityList = findViewById(R.id.city_list);
+        String[] cities = {"Edmonton", "Vancouver"};
+
+        dataList = new ArrayList<>();
+        dataList.addAll(Arrays.asList(cities));
+
+        cityAdapter = new ArrayAdapter<>(this, R.layout.content, dataList);
+        cityList.setAdapter(cityAdapter);
+
+        dataList.add("calgary");
+        cityAdapter.notifyDataSetChanged(); //tells to make changes visually
+
+        cityList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                dataList.remove(position);
+                cityAdapter.notifyDataSetChanged();
+            }
+        });
+
+
+        Add_Button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                input_field = findViewById(R.id.input_city);
+                dataList.add(input_field.getText().toString());
+                cityAdapter.notifyDataSetChanged();
+
+            }
+        });
+
+
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+    }
+}
